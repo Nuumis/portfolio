e88583510063d189c206a53b7d0a62935024b224
Vili-Petteri Maunu <vilipetterimaunu@gmail.com>
2023-02-05 17:17:16 +0200

    add login

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="css")
 
-@app.route("/")
-def index():
+@app.route("/userpage")
+def userpage():
     return render_template("index.html")
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+
 
 if __name__== "__main__":
     app.run(debug=True)
